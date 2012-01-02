4eb62bcc4adb36a22b3bcf1c2951f71c9818b90d
Andrey Kouznetsov <itsabear@ya.ru>
2012-01-02 04:47:58 +0400

    changed akka dependency to 2.0-M2 version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,27 +8,27 @@ object Dependencies {
   )
 
   object V {
-    val akka      = "1.3-RC4"
+    val akka      = "2.0-M2"
     val logback   = "0.9.29"
   }
 
   object Compile {
     val slf4j         = "org.slf4j"                 %   "slf4j-api"                 % "1.6.1"     % "compile"
-    val akkaActor     = "se.scalablesolutions.akka" %   "akka-actor"                % V.akka      % "compile"
+    val akkaActor     = "com.typesafe.akka" %   "akka-actor"                % V.akka      % "compile"
   }
 
   object Provided {
-    val akkaActor     = "se.scalablesolutions.akka" %   "akka-actor"                % V.akka      % "provided"
+    val akkaActor     = "com.typesafe.akka" %   "akka-actor"                % V.akka      % "provided"
   }
 
   object Test {
     val specs2        = "org.specs2"                %%  "specs2"                    % "1.6.1"     % "test"
-    val akkaSlf4j     = "se.scalablesolutions.akka" %   "akka-slf4j"                % V.akka      % "test"
+    val akkaSlf4j     = "com.typesafe.akka" %   "akka-slf4j"                % V.akka      % "test"
     val logback       = "ch.qos.logback"            %   "logback-classic"           % V.logback   % "test"
   }
 
   object Runtime {
-    val akkaSlf4j     = "se.scalablesolutions.akka" %   "akka-slf4j"                % V.akka      % "runtime"
+    val akkaSlf4j     = "com.typesafe.akka" %   "akka-slf4j"                % V.akka      % "runtime"
     val logback       = "ch.qos.logback"            %   "logback-classic"           % V.logback   % "runtime"
   }
 
