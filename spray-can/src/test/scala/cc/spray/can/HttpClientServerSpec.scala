e2471406215234ddc4f24ce59ac9891e6107944f
Andrey Kouznetsov <itsabear@ya.ru>
2012-01-09 21:28:57 +0400

    using ActorSystem for actors creation in HttpClientServerSpec.scala

--- spray-can/src/test/scala/cc/spray/can/HttpClientServerSpec.scala
+++ spray-can/src/test/scala/cc/spray/can/HttpClientServerSpec.scala
@@ -15,39 +15,38 @@
  */
 
 package cc.spray.can
-
-import org.specs2._
-import specification.Step
-import akka.actor.{Scheduler, Actor}
-import akka.util.Duration
 import HttpMethods._
-import java.util.concurrent.{CountDownLatch, TimeUnit}
-import java.util.concurrent.atomic.AtomicLong
+import akka.actor.{Actor, Props, Scheduler}
+import akka.util.Duration
+import java.util.concurrent.CountDownLatch
+import org.specs2._
+import org.specs2.specification.Step
 
-class HttpClientServerSpec extends Specification with HttpClientSpecs { def is =
+class HttpClientServerSpec extends Specification with HttpClientSpecs {
+  def is =
 
-  sequential^
-  "This spec exercises one HttpClient and one HttpServer instance"    ^
-  "with various request/response patterns"                            ^
-                                                                      p^
-                                                                      Step(start())^
-  "simple one-request dialog"                                         ! oneRequestDialog^
-  "one-request dialog with HTTP/1.0 terminated-by-close reponse"      ! terminatedByCloseDialog^
-  "request-response dialog"                                           ! requestResponse^
-  "non-pipelined request-request dialog"                              ! nonPipelinedRequestRequest^
-  "pipelined request-request dialog"                                  ! pipelinedRequestRequest^
-  "pipelined request-request dialog with response reordering"         ! responseReorderingDialog^
-  "multi-request pipelined dialog with response reordering"           ! multiRequestDialog^
-  "pipelined request-request dialog with HEAD requests"               ! pipelinedRequestsWithHead^
-  "one-request dialog with a chunked response"                        ! oneRequestChunkedResponse^
-  "one-request dialog with a chunked request"                         ! oneRequestChunkedRequest^
-  "pipelined requests dialog with one chunked request"                ! pipelinedRequestsWithChunkedRequest^
-  "pipelined requests dialog with one chunked response"               ! pipelinedRequestsWithChunkedResponse^
-  "time-out request"                                                  ! timeoutRequest^
-  "idle-time-out connection"                                          ! timeoutConnection^
-                                                                      p^
-  clientSpecs                                                         ^
-                                                                      Step(Actor.registry.shutdownAll())
+    sequential ^
+      "This spec exercises one HttpClient and one HttpServer instance" ^
+      "with various request/response patterns" ^
+      p ^
+      Step(start()) ^
+      "simple one-request dialog" ! oneRequestDialog ^
+      "one-request dialog with HTTP/1.0 terminated-by-close reponse" ! terminatedByCloseDialog ^
+      "request-response dialog" ! requestResponse ^
+      "non-pipelined request-request dialog" ! nonPipelinedRequestRequest ^
+      "pipelined request-request dialog" ! pipelinedRequestRequest ^
+      "pipelined request-request dialog with response reordering" ! responseReorderingDialog ^
+      "multi-request pipelined dialog with response reordering" ! multiRequestDialog ^
+      "pipelined request-request dialog with HEAD requests" ! pipelinedRequestsWithHead ^
+      "one-request dialog with a chunked response" ! oneRequestChunkedResponse ^
+      "one-request dialog with a chunked request" ! oneRequestChunkedRequest ^
+      "pipelined requests dialog with one chunked request" ! pipelinedRequestsWithChunkedRequest ^
+      "pipelined requests dialog with one chunked response" ! pipelinedRequestsWithChunkedResponse ^
+      "time-out request" ! timeoutRequest ^
+      "idle-time-out connection" ! timeoutConnection ^
+      p ^
+      clientSpecs ^
+      Step(Actor.registry.shutdownAll())
 
   private class TestService extends Actor {
     self.id = "server-test-server"
@@ -56,28 +55,32 @@ class HttpClientServerSpec extends Specification with HttpClientSpecs { def is =
       case RequestContext(HttpRequest(_, "/delayResponse", _, _, _), _, responder) =>
         delayedResponse = responder
       case RequestContext(HttpRequest(_, "/getThisAndDelayedResponse", _, _, _), _, responder) =>
-        responder.complete(HttpResponse().withBody("secondResponse"))          // first complete the second request
-        delayedResponse.complete(HttpResponse().withBody("delayedResponse"))  // then complete the first request
+        responder.complete(HttpResponse().withBody("secondResponse")) // first complete the second request
+        delayedResponse.complete(HttpResponse().withBody("delayedResponse")) // then complete the first request
       case RequestContext(HttpRequest(_, path, _, _, _), _, responder) if path.startsWith("/multi/") =>
         val delay = (scala.math.random * 80.0).toLong
-        Scheduler.scheduleOnce(() => responder.complete(HttpResponse().withBody(path.last.toString)), delay, TimeUnit.MILLISECONDS)
+        context.system.scheduler.scheduleOnce(Duration.create(delay, "millis")) {
+          responder.complete(HttpResponse().withBody(path.last.toString))
+        }
       case RequestContext(HttpRequest(_, "/chunked", _, _, _), _, responder) => {
         val latch = new CountDownLatch(1)
         val chunker = responder.startChunkedResponse(HttpResponse(201, List(HttpHeader("Fancy", "cool"))))
         chunker.sendChunk(MessageChunk("1")).onSuccess { case () =>
-          chunker.sendChunk(MessageChunk("-2345")).onSuccess { case () =>
-            chunker.sendChunk(MessageChunk("-6789ABCD")).onSuccess { case () =>
-              chunker.sendChunk(MessageChunk("-EFGHIJKLMNOPQRSTUVWXYZ")).onSuccess { case () =>
-                latch.countDown()
-              }
+            chunker.sendChunk(MessageChunk("-2345")).onSuccess { case () =>
+                chunker.sendChunk(MessageChunk("-6789ABCD")).onSuccess { case () =>
+                    chunker.sendChunk(MessageChunk("-EFGHIJKLMNOPQRSTUVWXYZ")).onSuccess { case () =>
+                        latch.countDown()
+                    }
+                }
             }
-          }
         }
         latch.await()
         chunker.close()
       }
       case RequestContext(HttpRequest(_, "/wait200", _, _, _), _, responder) =>
-        Scheduler.scheduleOnce(() => responder.complete(HttpResponse()), 200, TimeUnit.MILLISECONDS)
+        context.system.scheduler.scheduleOnce(Duration.create(200, "millis")) {
+          responder.complete(HttpResponse())
+        }
       case RequestContext(HttpRequest(_, "/terminatedByClose", _, _, _), _, responder) => responder.complete {
         HttpResponse(protocol = HttpProtocols.`HTTP/1.0`).withBody("This body is terminated by closing the connection!")
       }
@@ -93,140 +96,139 @@ class HttpClientServerSpec extends Specification with HttpClientSpecs { def is =
 
   private def oneRequestDialog = {
     dialog
-            .send(HttpRequest(uri = "/yeah"))
-            .end
-            .get.bodyAsString mustEqual "GET|/yeah"
+      .send(HttpRequest(uri = "/yeah"))
+      .end
+      .get.bodyAsString mustEqual "GET|/yeah"
   }
 
   private def terminatedByCloseDialog = {
     dialog
-            .send(HttpRequest(uri = "/terminatedByClose"))
-            .end
-            .get.bodyAsString mustEqual "This body is terminated by closing the connection!"
+      .send(HttpRequest(uri = "/terminatedByClose"))
+      .end
+      .get.bodyAsString mustEqual "This body is terminated by closing the connection!"
   }
 
   private def requestResponse = {
     def respond(res: HttpResponse) = HttpRequest(POST).withBody("(" + res.bodyAsString + ")")
 
     dialog
-            .send(HttpRequest(GET, "/abc"))
-            .reply(respond)
-            .end
-            .get.bodyAsString mustEqual "POST|/|(GET|/abc)"
+      .send(HttpRequest(GET, "/abc"))
+      .reply(respond)
+      .end
+      .get.bodyAsString mustEqual "POST|/|(GET|/abc)"
   }
 
   private def nonPipelinedRequestRequest = {
-      dialog
-              .send(HttpRequest(DELETE, "/abc"))
-              .awaitResponse
-              .send(HttpRequest(PUT, "/xyz"))
-              .end
-              .get.map(_.bodyAsString).mkString(", ") mustEqual "DELETE|/abc, PUT|/xyz"
-    }
+    dialog
+      .send(HttpRequest(DELETE, "/abc"))
+      .awaitResponse
+      .send(HttpRequest(PUT, "/xyz"))
+      .end
+      .get.map(_.bodyAsString).mkString(", ") mustEqual "DELETE|/abc, PUT|/xyz"
+  }
 
   private def pipelinedRequestRequest = {
     dialog
-            .send(HttpRequest(DELETE, "/abc"))
-            .send(HttpRequest(PUT, "/xyz"))
-            .end
-            .get.map(_.bodyAsString).mkString(", ") mustEqual "DELETE|/abc, PUT|/xyz"
+      .send(HttpRequest(DELETE, "/abc"))
+      .send(HttpRequest(PUT, "/xyz"))
+      .end
+      .get.map(_.bodyAsString).mkString(", ") mustEqual "DELETE|/abc, PUT|/xyz"
   }
 
   private def responseReorderingDialog = {
     dialog
-            .send(HttpRequest(uri = "/delayResponse"))
-            .send(HttpRequest(uri = "/getThisAndDelayedResponse"))
-            .end
-            .get.map(_.bodyAsString).mkString(",") mustEqual "delayedResponse,secondResponse"
+      .send(HttpRequest(uri = "/delayResponse"))
+      .send(HttpRequest(uri = "/getThisAndDelayedResponse"))
+      .end
+      .get.map(_.bodyAsString).mkString(",") mustEqual "delayedResponse,secondResponse"
   }
 
   private def multiRequestDialog = {
     dialog
-            .send((1 to 9).map(i => HttpRequest(uri = "/multi/" + i)))
-            .end
-            .get.map(_.bodyAsString).mkString(",") mustEqual "1,2,3,4,5,6,7,8,9"
+      .send((1 to 9).map(i => HttpRequest(uri = "/multi/" + i)))
+      .end
+      .get.map(_.bodyAsString).mkString(",") mustEqual "1,2,3,4,5,6,7,8,9"
   }
 
   private def pipelinedRequestsWithHead = {
     dialog
-            .send(HttpRequest(DELETE, "/abc"))
-            .send(HttpRequest(HEAD, "/def"))
-            .send(HttpRequest(PUT, "/xyz"))
-            .end
-            .get.map { r =>
-              (r.headers.collect({ case HttpHeader("Content-Length", cl) => cl }).head.toInt, r.bodyAsString)
-            } mustEqual Seq((11, "DELETE|/abc"), (9, ""), (8, "PUT|/xyz"))
+      .send(HttpRequest(DELETE, "/abc"))
+      .send(HttpRequest(HEAD, "/def"))
+      .send(HttpRequest(PUT, "/xyz"))
+      .end
+      .get.map { r =>
+        (r.headers.collect({ case HttpHeader("Content-Length", cl) => cl }).head.toInt, r.bodyAsString)
+      } mustEqual Seq((11, "DELETE|/abc"), (9, ""), (8, "PUT|/xyz"))
   }
 
   private def oneRequestChunkedResponse = {
     dialog
-            .send(HttpRequest(GET, "/chunked"))
-            .end
-            .get.bodyAsString mustEqual "1-2345-6789ABCD-EFGHIJKLMNOPQRSTUVWXYZ"
+      .send(HttpRequest(GET, "/chunked"))
+      .end
+      .get.bodyAsString mustEqual "1-2345-6789ABCD-EFGHIJKLMNOPQRSTUVWXYZ"
   }
 
   private def oneRequestChunkedRequest = {
     dialog
-            .sendChunked(HttpRequest(GET)) { chunker =>
-              chunker.sendChunk(MessageChunk("1"))
-              chunker.sendChunk(MessageChunk("2"))
-              chunker.sendChunk(MessageChunk("3"))
-              chunker.close()
-            }
-            .end
-            .get.bodyAsString mustEqual "GET|/|123"
+      .sendChunked(HttpRequest(GET)) { chunker =>
+        chunker.sendChunk(MessageChunk("1"))
+        chunker.sendChunk(MessageChunk("2"))
+        chunker.sendChunk(MessageChunk("3"))
+        chunker.close()
+      }
+      .end
+      .get.bodyAsString mustEqual "GET|/|123"
   }
 
   private def pipelinedRequestsWithChunkedRequest = {
     dialog
-            .send(HttpRequest(DELETE, "/delete"))
-            .sendChunked(HttpRequest(PUT, "/put")) { chunker =>
-              chunker.sendChunk(MessageChunk("1"))
-              chunker.sendChunk(MessageChunk("2"))
-              chunker.sendChunk(MessageChunk("3"))
-              chunker.close()
-            }
-            .send(HttpRequest(GET, "/get"))
-            .end
-            .get.map(_.bodyAsString).mkString(", ") mustEqual "DELETE|/delete, PUT|/put|123, GET|/get"
+      .send(HttpRequest(DELETE, "/delete"))
+      .sendChunked(HttpRequest(PUT, "/put")) { chunker =>
+        chunker.sendChunk(MessageChunk("1"))
+        chunker.sendChunk(MessageChunk("2"))
+        chunker.sendChunk(MessageChunk("3"))
+        chunker.close()
+      }
+      .send(HttpRequest(GET, "/get"))
+      .end
+      .get.map(_.bodyAsString).mkString(", ") mustEqual "DELETE|/delete, PUT|/put|123, GET|/get"
   }
 
   private def pipelinedRequestsWithChunkedResponse = {
     dialog
-            .send(HttpRequest(POST, "/"))
-            .send(HttpRequest(GET, "/chunked"))
-            .send(HttpRequest(PUT, "/xyz"))
-            .end
-            .get.map(_.bodyAsString).mkString(", ") mustEqual "POST|/, 1-2345-6789ABCD-EFGHIJKLMNOPQRSTUVWXYZ, PUT|/xyz"
+      .send(HttpRequest(POST, "/"))
+      .send(HttpRequest(GET, "/chunked"))
+      .send(HttpRequest(PUT, "/xyz"))
+      .end
+      .get.map(_.bodyAsString).mkString(", ") mustEqual "POST|/, 1-2345-6789ABCD-EFGHIJKLMNOPQRSTUVWXYZ, PUT|/xyz"
   }
 
   private def timeoutRequest = {
     dialog
-            .send(HttpRequest(uri = "/wait200"))
-            .end
-            .get.bodyAsString mustEqual "TIMEOUT"
+      .send(HttpRequest(uri = "/wait200"))
+      .end
+      .get.bodyAsString mustEqual "TIMEOUT"
   }
 
   private def timeoutConnection = {
     dialog
-            .waitIdle(Duration("500 ms"))
-            .send(HttpRequest())
-            .end
-            .await.exception.get.getMessage mustEqual "Cannot send request due to closed connection"
+      .waitIdle(Duration("500 ms"))
+      .send(HttpRequest())
+      .end
+      .await.exception.get.getMessage mustEqual "Cannot send request due to closed connection"
   }
 
   private def dialog = HttpDialog(host = "localhost", port = 17242, clientActorId = "server-test-client")
 
   private def start() {
-    Actor.actorOf(new TestService).start()
-    Actor.actorOf(new HttpServer(ServerConfig(
+    system.actorOf(Props(new TestService))
+    system.actorOf(Props(new HttpServer(ServerConfig(
       port = 17242,
       serviceActorId = "server-test-server",
       timeoutActorId = "server-test-server",
       requestTimeout = 100, timeoutCycle = 50,
-      idleTimeout = 200, reapingCycle = 100
-    ))).start()
-    Actor.actorOf(new HttpClient(ClientConfig(clientActorId = "server-test-client", requestTimeout = 1000))).start()
+      idleTimeout = 200, reapingCycle = 100))))
+    system.actorOf(Props(new HttpClient(ClientConfig(clientActorId = "server-test-client", requestTimeout = 1000))))
   }
 }
 
